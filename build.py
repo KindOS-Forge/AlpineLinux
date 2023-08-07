cd3141167029de156ce42a424ec0237d818d8821
João Pinto <lamego.pinto@gmail.com>
2023-08-07 18:07:11 +0200

    Prepare for recreation

--- build.py
+++ build.py
@@ -2,6 +2,14 @@
 import os
 import sys
 
+
+
 ALPINE_VERSION = "3.12.2"
 ALPINE_ARCH = "x86_64"
 ALPINE_MIRROR = "http://dl-cdn.alpinelinux.org/alpine"
+
+short_version = ALPINE_VERSION.split(".")
+
+downoad_url = f"{ALPINE_MIRROR}/v{ALPINE_VERSION}/releases/{ALPINE_ARCH}/alpine-minirootfs-{version}-{arch}.tar.gz"
+
+curl -O https://dl-cdn.alpinelinux.org/alpine/v${ALPINE_VERSION}/releases/x86_64/alpine-minirootfs-${ALPINE_VERSION}${ALPINE_FIX}-x86_64.tar.gz
