8d137aceae4c144dcf008e3e097ad62ef6a573ac
João Pinto <lamego.pinto@gmail.com>
2023-08-07 16:20:16 +0200

    remove the pre clean rm

--- build.sh
+++ build.sh
@@ -13,13 +13,15 @@ curl -O https://dl-cdn.alpinelinux.org/alpine/v${ALPINE_VERSION}/releases/x86_64
 # Import the image into docker
 echo "Importing the image into docker..."
 docker import - kindos-alpinelinux < alpine-minirootfs-${ALPINE_VERSION}${ALPINE_FIX}-x86_64.tar.gz
-docker rm kindos || true
 docker run -v ${SCRIPT_DIR}/kindos:/kindos \
     --name kindos \
     -it kindos-alpinelinux \
     sh -c "apk add python3 && python -m kindos"
 
 docker export kindos > kindos-alpinelinux.tar
+docker rm kindos
+docker rmi kindos-alpinelinux
+
 xz kindos-alpinelinux.tar
 du -sh kindos-alpinelinux.tar.xz
 
