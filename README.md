ab8ae6b3d652ca959ecb05a23429c8efc51a4906
João Pinto <lamego.pinto@gmail.com>
2023-08-07 21:52:08 +0200

    check the new python version

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# The KindOS Project
-
-## What is KindOS?
-
-KindOS is a project to create a minimal Linux distribution to use for opensource software development. 
-It is based on Alpine Linux and is designed to be as small as possible while still being useful.
-KindOS image for the Alpine Linux flavor
+# example-package
--- build.py
+++ build.py
@@ -1,11 +1,10 @@
 #!/bin/python3
 import os
 import sys
-from tempfile import NamedTemporaryFile
-
-
-import tempfile
+from tempfile import TemporaryDirectory
+import urllib
 import urllib.request
+from pathlib import Path
 
 ALPINE_VERSION = "3.18.2"
 ALPINE_ARCH = "x86_64"
@@ -16,12 +15,31 @@ short_version = '.'.join(ALPINE_VERSION.split(".")[:2])
 #  BAD: https://dl-cdn.alpinelinux.org/alpine/v3.18.2/releases/x86_64/alpine-minirootfs-3.18-x86_64.tar.gz
 downoad_url = f"{ALPINE_MIRROR}/v{short_version}/releases/{ALPINE_ARCH}/alpine-minirootfs-{ALPINE_VERSION}-{ALPINE_ARCH}.tar.gz"
 
+def human_readable_size(size: int) -> str:
+    """ Returns human readable size """
+    if size < 1024:
+        return f"{size}B"
+    size /= 1024
+    if size < 1024:
+        return f"{size:.2f}KB"
+    size /= 1024
+    if size < 1024:
+        return f"{size:.2f}MB"
+    size /= 1024
+    return f"{size:.2f}GB"
 
-# Create a temporary file to store the downloaded content
-with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-    temp_file_name = temp_file.name
+def execute_command(cmd: str, ignore_errors: bool = False):
+    """ Execute a command and raises exception if it fails """
+    if os.system(cmd) != 0:
+        if ignore_errors:
+            return
+        raise Exception(f"Failed to execute command:\n {cmd}")
 
-    # Download the file and write its content to the temporary file
+tmp = TemporaryDirectory()
+temp_file_name = Path(tmp.name) / "alpine.tar.gz"
+
+# Download the file and write its content to the temporary file
+with open(temp_file_name, "wb") as temp_file:
     try:
         with urllib.request.urlopen(downoad_url) as response:
                 temp_file.write(response.read())
@@ -29,10 +47,32 @@ with tempfile.NamedTemporaryFile(delete=False) as temp_file:
         print(downoad_url, file=sys.stderr)
         raise Exception(f"Failed to download from {downoad_url}, error: {e}")
 
-    temp_file.flush()
 
 # Now you can use the content in the temporary file as needed
 print(f"File downloaded and stored in: {temp_file_name}")
-os.system(f"docker import - kindos-alpinelinux < {temp_file_name}")
+execute_command(f"docker import - kindos < {temp_file_name}")
+
+execute_command(f"docker rm kindos", ignore_errors=True)
 
 
+cwd = os.getcwd()
+cmd = f""" docker run \
+    -v {cwd}/kindos-deploy:/kindos-deploy:ro \
+    --name kindos \
+    kindos \
+    sh -c 'apk update && apk add python3 && /kindos-deploy/deploy.py'
+"""
+execute_command(cmd)
+temp_file_name = Path(tmp.name) / "alpine.tar"
+execute_command(f"docker export -o {temp_file_name} kindos")
+
+gzip_temp_file_name = Path(tmp.name) / "alpine.tar.gz"
+
+# Use execute command to gzip the file because it is much faster than python gzip
+#   ~= 4s with system gzip vs 8s with python gzip
+execute_command(f"gzip {gzip_temp_file_name} --force")
+execute_command(f"du -sh {gzip_temp_file_name}.gz")
+size = os.path.getsize(f"{gzip_temp_file_name}.gz")
+print(f"Compressed size: {human_readable_size(size)}")
+os.makedirs("dist", exist_ok=True)
+execute_command(f"mv {gzip_temp_file_name}.gz dist/kindos.tar.gz")
